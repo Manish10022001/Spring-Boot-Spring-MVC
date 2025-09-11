f497b7dc89b0a58185e670052574dff370c05ce9
Manish10022001 <manishshirsat2850@gmail.com>
2025-09-11 11:40:18 +0530

    validated to make integer fields required

---
+++ 16-validationdemo-make-integer-fields-required/src/main/java/com/springboot/mvc/model/Customer.java
@@ -0,0 +1,57 @@
+package com.springboot.mvc.model;
+
+import jakarta.validation.constraints.*;
+
+//1.1
+public class Customer {
+    private String firstName;
+
+    @NotNull(message = "is required")
+    @Size(min=1, message = "is required")
+    private String lastName;
+
+    @NotNull(message = "is required")
+    @Min(value = 0, message = "must be greater than or equal to zero")
+    @Max(value=10, message = "must be less that or equal to 10")
+//    private int freePasses; //3.1
+    private Integer freePasses; //5.1
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "Only 5 chars/digits")
+    private String postalCode; //4.1
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    /* 3.1
+    public int getFreePasses() {
+        return freePasses;
+    }
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
+    */
+    //5.2
+    public Integer getFreePasses() {
+        return freePasses;
+    }
+    public void setFreePasses(Integer freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+}
